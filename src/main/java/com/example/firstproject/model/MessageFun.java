f1e30a79ec336b23a637ae63c21ad3e204309b2f
kirill shcherba <kirillshcherba92@gmail.com>
2021-11-09 01:36:30 +0300

    Разнёс footer и header. Добавил сущность MessageFun. Добавлен контроллер для записи новых сообщений

---
+++ src/main/java/com/example/firstproject/model/MessageFun.java
@@ -0,0 +1,57 @@
+package com.example.firstproject.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "jokes")
+public class MessageFun {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+    private String title;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id")
+    private Users author;
+
+    //*
+    // Не уверен, но возможно лайки и дизлайки можно нужно здесь ставить в этой базе данных
+    // *//
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Users getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Users author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/example/firstproject/repository/MessageFunRepository.java
@@ -0,0 +1,8 @@
+package com.example.firstproject.repository;
+
+import com.example.firstproject.model.MessageFun;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageFunRepository extends JpaRepository<MessageFun, Long> {
+
+}
